1c6460c602b18c6b9a60755a0854f8e0cb2baad8
mikhailshaganov <81769678+mikhailshaganov@users.noreply.github.com>
2021-05-04 17:30:53 +0300

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -2,18 +2,18 @@
 #!/bin/bash -e
 
 # Stop and disable apt-daily upgrade services;
-sudo systemctl stop apt-daily.timer
-sudo systemctl disable apt-daily.timer
-sudo systemctl disable apt-daily.service
-sudo systemctl stop apt-daily-upgrade.timer
-sudo systemctl disable apt-daily-upgrade.timer
-sudo systemctl disable apt-daily-upgrade.service
+systemctl stop apt-daily.timer
+systemctl disable apt-daily.timer
+systemctl disable apt-daily.service
+systemctl stop apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily-upgrade.service
 
 # Enable retry logic for apt up to 10 times
-sudo echo "APT::Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries
+echo "APT::Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries
 
 # Configure apt to always assume Y
-sudo echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
+echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
 # Uninstall unattended-upgrades
 apt-get purge unattended-upgrades
@@ -31,10 +31,10 @@ sudo rm -r /etc/apt/sources.list.d/apt-fast-*
 
 # Need to limit arch for default apt repos due to 
 # https://github.com/actions/virtual-environments/issues/1961
-sudo sed -i'' -E 's/^deb http:\/\/(azure.archive|security).ubuntu.com/deb [arch=amd64,i386] http:\/\/\1.ubuntu.com/' /etc/apt/sources.list
+sed -i'' -E 's/^deb http:\/\/(azure.archive|security).ubuntu.com/deb [arch=amd64,i386] http:\/\/\1.ubuntu.com/' /etc/apt/sources.list
 
-sudo echo 'APT sources limited to the actual architectures'
-sudo cat /etc/apt/sources.list
+echo 'APT sources limited to the actual architectures'
+cat /etc/apt/sources.list
 
 apt-get update
 # Install aria2 , jq and apt-fast
