b62a7642008fa3bcae92ff97273323f60e967a17
mikhailshaganov <81769678+mikhailshaganov@users.noreply.github.com>
2021-05-05 11:12:17 +0300

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -16,7 +16,7 @@ echo "APT::Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
 # Uninstall unattended-upgrades
-apt-get purge unattended-upgrades
+sudo apt-get purge apt-fast
 
 # Use apt-fast for parallel downloads
 # deb http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main 
@@ -24,17 +24,17 @@ apt-get purge unattended-upgrades
 # apt-add-repository $REPO_URL -y
 bash -c "$(curl -sL https://git.io/vokNn)"
 
-apt-get update
+# apt-get update
 
-sudo rm -r /etc/apt/sources.list.d/apt-fast-*
-# add-apt-repository -y ppa:apt-fast/stable
+# sudo rm -r /etc/apt/sources.list.d/apt-fast-*
+# # add-apt-repository -y ppa:apt-fast/stable
 
-# Need to limit arch for default apt repos due to 
-# https://github.com/actions/virtual-environments/issues/1961
-sed -i'' -E 's/^deb http:\/\/(azure.archive|security).ubuntu.com/deb [arch=amd64,i386] http:\/\/\1.ubuntu.com/' /etc/apt/sources.list
+# # Need to limit arch for default apt repos due to 
+# # https://github.com/actions/virtual-environments/issues/1961
+# sed -i'' -E 's/^deb http:\/\/(azure.archive|security).ubuntu.com/deb [arch=amd64,i386] http:\/\/\1.ubuntu.com/' /etc/apt/sources.list
 
-echo 'APT sources limited to the actual architectures'
-cat /etc/apt/sources.list
+# echo 'APT sources limited to the actual architectures'
+# cat /etc/apt/sources.list
 
 apt-get update
 # Install aria2 , jq and apt-fast
