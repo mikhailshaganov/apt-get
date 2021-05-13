15270e586a2624c2362e830d7169a27e3e12211f
mikhailshaganov <81769678+mikhailshaganov@users.noreply.github.com>
2021-05-13 13:00:52 +0300

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,4 +1,3 @@
-  
 #!/bin/bash -e
 
 # Stop and disable apt-daily upgrade services;
@@ -16,26 +15,19 @@ echo "APT::Acquire::Retries \"10\";" > /etc/apt/apt.conf.d/80-retries
 echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
 # Uninstall unattended-upgrades
-sudo apt-get purge apt-fast
+apt-get purge unattended-upgrades
 
-# Use apt-fast for parallel downloads
-# deb http://ppa.launchpad.net/apt-fast/stable/ubuntu bionic main 
-# REPO_URL="ppa:apt-fast/stable"
-# apt-add-repository $REPO_URL -y
-bash -c "$(curl -sL https://git.io/vokNn)"
+# Need to limit arch for default apt repos due to 
+# https://github.com/actions/virtual-environments/issues/1961
+sed -i'' -E 's/^deb http:\/\/(azure.archive|security).ubuntu.com/deb [arch=amd64,i386] http:\/\/\1.ubuntu.com/' /etc/apt/sources.list
 
-# apt-get update
-
-# sudo rm -r /etc/apt/sources.list.d/apt-fast-*
-# # add-apt-repository -y ppa:apt-fast/stable
-
-# # Need to limit arch for default apt repos due to 
-# # https://github.com/actions/virtual-environments/issues/1961
-# sed -i'' -E 's/^deb http:\/\/(azure.archive|security).ubuntu.com/deb [arch=amd64,i386] http:\/\/\1.ubuntu.com/' /etc/apt/sources.list
-
-# echo 'APT sources limited to the actual architectures'
-# cat /etc/apt/sources.list
+echo 'APT sources limited to the actual architectures'
+cat /etc/apt/sources.list
 
 apt-get update
-# Install aria2 , jq and apt-fast
-apt-get install aria2 jq 
+# Install aria2 , jq
+apt-get install aria2 jq
+
+# Install apt-fast using quick-install.sh
+# https://github.com/ilikenwf/apt-fast
+bash -c "$(curl -sL https://raw.githubusercontent.com/ilikenwf/apt-fast/master/quick-install.sh)"
